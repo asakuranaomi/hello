97768893a08706e4a7406a6c934c74404a37d7ff
chua.joanne <pk-22130006@Joanne-no-MacBook-Pro.local>
2015-11-13 12:44:58 +0900

    edit base POT

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(void)
     setlocale( LC_ALL, "" );
     bindtextdomain( "hello", "/usr/share/locale" );
     textdomain( "hello" );
-    printf( gettext( "Hello, world!\n" ) );
+    printf( gettext( "What up, world!\n" ) );
     printf( ngettext( "Orangutan has %d banana.\n", "Orangutan has %d bananas.\n", count ), count );
     printf( gettext( "Try Weblate at <http://demo.weblate.org/>!\n" ) );
     printf( "%s\n", gettext( "Thank you for using Weblate." ) );
